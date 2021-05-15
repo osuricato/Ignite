01d9ad305d790a420c8dff8c9ebc97a3de58d37f
samuel.gomes <samuel.gomes@precato.com.br>
2021-05-15 18:39:24 -0300

    debugando a aplicação

--- Chapter 2/rentalx/src/server.ts
+++ Chapter 2/rentalx/src/server.ts
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello world!" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
 app.listen(3333, () => {
   console.log("Server started on port 3333.");
 });
