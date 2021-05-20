fa4fdbcb73daab6ec93141ca86313171c0892704
samuel.gomes <samuel.gomes@precato.com.br>
2021-05-20 17:06:49 -0300

    resolving promises

--- Chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ Chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,9 +4,12 @@ import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
   handle(request: Request, response: Response): Response {
     const { file } = request;
-    console.log(file);
+
+    this.importCategoryUseCase.execute(file);
+
     return response.send();
   }
 }
--- Chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,44 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategorie {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  constructor(private categoriesRepositories: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategorie[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategorie[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
   }
 }
 
