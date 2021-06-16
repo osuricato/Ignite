8dd911ed82a86952b9b2380902ef581d91ab3ac9
osuricato <samuel.souzagomes001@gmail.com>
2021-06-16 08:35:55 -0300

    autenticação de usuário

---
+++ rentx/src/routes/authenticate.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { AuthenticateUserUseController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AuthenticateUserUseController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRoutes };
