b50e51eb5543278ae3c2309e441861cb74a01916
osuricato <samuel.souzagomes001@gmail.com>
2021-06-23 08:58:45 -0300

    refactoring app

--- rentx/src/shared/infra/http/routes/authenticate.routes.ts
+++ rentx/src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { AuthenticateUserUseController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { AuthenticateUserUseController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 
 const authenticateRoutes = Router();
 
--- rentx/src/shared/infra/http/routes/specifications.routes.ts
+++ rentx/src/shared/infra/http/routes/specifications.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { ensureAuthenticated } from "../middleware/ensureAuthenticate";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAuthenticated } from "@shared/infra/http/ensureAuthenticate";
 
 const specificationsRoutes = Router();
 
