c4b15ea3cc4b85179b23da3d916a74760db3d98e
osuricato <samuel.souzagomes001@gmail.com>
2021-07-02 08:45:05 -0300

    set false available car when is rental

--- rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,8 +1,10 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject, injectable } from "tsyringe";
 
 interface IRequest {
   user_id: string;
@@ -12,10 +14,12 @@ interface IRequest {
 @injectable()
 class CreateRentalUseCase {
   constructor(
-    @inject('RentalsRepository')
+    @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
-    @inject('DayjsDateProvider')
-    private dateProvider: IDateProvider
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) {}
 
   async execute({
@@ -57,6 +61,8 @@ class CreateRentalUseCase {
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
